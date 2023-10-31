5ab8d8791e367882f508635956997b877770816c
Programming Bees <hasanakash799@gmail.com>
2023-11-01 01:14:10 +0600

    collect static

--- static/js/main.js
+++ static/js/main.js
@@ -1,12 +1,3 @@
-/*  ---------------------------------------------------
-    Theme Name: Anime
-    Description: Anime video tamplate
-    Author: Colorib
-    Author URI: https://colorib.com/
-    Version: 1.0
-    Created: Colorib
----------------------------------------------------------  */
-
 'use strict';
 
 (function ($) {
@@ -15,7 +6,7 @@
         Preloader
     --------------------*/
     $(window).on('load', function () {
-        $(".loader").fadeOut();
+        $(".loader").fadeOut("slow");
         $("#preloder").delay(200).fadeOut("slow");
 
         /*------------------
@@ -71,7 +62,7 @@
         navText: ["<span class='arrow_carrot-left'></span>", "<span class='arrow_carrot-right'></span>"],
         animateOut: 'fadeOut',
         animateIn: 'fadeIn',
-        smartSpeed: 1200,
+        smartSpeed: 2000,
         autoHeight: false,
         autoplay: true,
         mouseDrag: false
@@ -98,4 +89,89 @@
         return false;
      });
 
+     
+    $(function(){
+        /************** Normal Breadcrumb vedio background *********/
+
+        function setVideoSize() {
+            const vidWidth = 1280;
+            const vidHeight = 720;
+            const maxVidHeight = 400;
+            let windowWidth = window.innerWidth;
+            let newVidWidth = windowWidth;
+            let newVidHeight = windowWidth * vidHeight / vidWidth;
+            let marginLeft = 0;
+            let marginTop = 0;
+        
+            if (newVidHeight < maxVidHeight) {
+                newVidHeight = maxVidHeight;
+                newVidWidth = newVidHeight * vidWidth / vidHeight;
+            }
+        
+            if(newVidWidth > windowWidth) {
+                marginLeft = -((newVidWidth - windowWidth) / 2);
+            }
+        
+            if(newVidHeight > maxVidHeight) {
+                marginTop = -((newVidHeight - $('#tm-video-container').height()) / 2);
+            }
+        
+            const tmVideo = $('#tm-video');
+        
+            tmVideo.css('width', newVidWidth);
+            tmVideo.css('height', newVidHeight);
+            tmVideo.css('margin-left', marginLeft);
+            tmVideo.css('margin-top', marginTop);
+        }
+
+        setVideoSize();
+
+        // Set video background size based on window size
+        let timeout;
+        window.onresize = function () {
+            clearTimeout(timeout);
+            timeout = setTimeout(setVideoSize, 100);
+        };
+
+        // Play/Pause button for video background      
+        const btn = $("#tm-video-control-button");
+
+        btn.on("click", function (e) {
+            const video = document.getElementById("tm-video");
+            $(this).removeClass();
+
+            if (video.paused) {
+                video.play();
+                $(this).addClass("fas fa-pause");
+            } else {
+                video.pause();
+                $(this).addClass("fas fa-play");
+            }
+        });
+    });
+
+        // Testimonials carousel
+        $(".quote-carousel").owlCarousel({
+            center: true,
+            autoplay: true,
+            smartSpeed: 1000,
+            dots: true,
+            loop: true,
+            responsive: {
+                0:{
+                    items:1
+                },
+                576:{
+                    items:1
+                },
+                768:{
+                    items:2
+                },
+                992:{
+                    items:3
+                }
+            }
+        });
+
 })(jQuery);
+
