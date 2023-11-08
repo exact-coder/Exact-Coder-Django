1b128fbf5a21af28c6ad56c4f3335fa721a6efd0
Programming Bees <hasanakash799@gmail.com>
2023-11-08 16:34:58 +0600

    chore: added see more button in home page

--- home/views.py
+++ home/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
+from protfolio.models import OurWork
+
 
 # Home Page View.
 def home(request):
-    return render(request,'pages/home.html')
+    ourworks = OurWork.objects.all().order_by('-pkid')[:6]
+    context = {
+        'ourworks':ourworks,
+    }
+    return render(request,'pages/home.html',context)
 
 # Login Page View.
 def login(request):
