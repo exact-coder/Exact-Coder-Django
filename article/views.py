e86e15fb5f85aab9f812670a854131a4059ba450
Programming Bees <hasanakash799@gmail.com>
2023-12-26 22:07:35 +0600

    chore: added previous and next article in article_details

--- article/views.py
+++ article/views.py
@@ -19,13 +19,17 @@ def articles(request):
     return render(request,'pages/articles.html',context)
 
 def article_details(request,slug):
-    article_obj = Article.objects.filter(slug=slug).first()
+    article_obj = get_object_or_404(Article,slug=slug)
     article_section = ArticleSection.objects.filter(article=article_obj)
-    print("===================")
-    print("===================")
+    categories = article_obj.categories.all()
+    # Get the previous and next posts within the same category
+    previous_article = Article.objects.filter(categories__in=categories, slug__lt=slug).order_by('-id').first()
+    next_article = Article.objects.filter(categories__in=categories, slug__gt=slug).order_by('id').first()
     context = {
         "article": article_obj,
         "article_extra_section":article_section,
+        'previous_article': previous_article,
+        'next_article': next_article,
     }
     return render(request,'pages/article_details.html',context)
 
