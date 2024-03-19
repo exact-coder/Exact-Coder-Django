fd819b18faae0a85cda153a7447c553af43f0b8e
Programming Bees <hasanakash799@gmail.com>
2024-03-19 21:00:27 +0600

    chore: default admin panel customize

--- exactcoder_root/urls.py
+++ exactcoder_root/urls.py
@@ -9,7 +9,7 @@ from django.views.static import serve
 urlpatterns = [
     re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
     re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls,name="admin"),
     path("",include("home.urls")),
     # path('accounts/', include('allauth.urls')),
     path("users/",include("accounts.urls")),
